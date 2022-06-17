69c5244b8daa8db65c60fa447b944878bdd52263
Aerafal13 <aerafal.grimhart@gmail.com>
2022-06-17 14:49:24 +0200

    Update GuildMemberAddedHandler.cs

--- src/HTS/Events/Handlers/GuildMemberAddedHandler.cs
+++ src/HTS/Events/Handlers/GuildMemberAddedHandler.cs
@@ -15,13 +15,16 @@ public sealed class GuildMemberAddedHandler : INotificationHandler<GuildMemberAd
 
 	public async Task Handle(GuildMemberAddedNotification notification, CancellationToken cancellationToken)
 	{
-		var welcomeChannel = notification.Args.Guild.GetChannel(_configuration.GetRequiredSection("channels").GetValue<ulong>("welcome"));
+		var welcomeChannel = notification.Args.Guild.GetChannel(
+			_configuration.GetRequiredSection("channels")
+			.GetValue<ulong>("welcome"));
+
 		var presentationChannelId = _configuration.GetRequiredSection("channels").GetValue<ulong>("presentation");
 
 		var embed = new DiscordEmbedBuilder()
 			.WithAuthor(notification.Args.Member.Username, iconUrl: notification.Args.Member.GetAvatarUrl(ImageFormat.Png))
 			.WithDescription($@"Bonjour {notification.Args.Member.Mention} :wave: et bienvenue sur {notification.Args.Guild.Name}.
-							N'hésite pas à te présenter dans le salon <#{presentationChannelId}>!")
+			N'hésite pas à te présenter dans le salon <#{presentationChannelId}>!")
 			.WithColor(DiscordColor.Orange)
 			.Build();
 
